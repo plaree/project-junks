9c79b037b7aa8ac1432d09a897d877e3b8f54741
Eralp Demirer <eralp0814@gmail.com>
2023-04-11 22:43:43 +0300

    Create cascade.py

---
+++ cascade.py
@@ -0,0 +1,22 @@
+import cv2
+
+# Cascade sınıflandırıcısı yüklenir
+car_cascade = cv2.CascadeClassifier('cars.xml')
+
+# Görüntü okunur
+img = cv2.imread('4.png')
+
+# Görüntü grayscale olarak dönüştürülür
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Araçlar tespit edilir
+cars = car_cascade.detectMultiScale(gray, 1.1, 3)
+
+# Her bir araç için dikdörtgen çizilir
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+# Sonuç görüntüsü gösterilir
+cv2.imshow('Cars Detected', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
